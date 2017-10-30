1457f452eefbf6e2a04736413d6965adabe53b82
Ben Keen <ben.keen@gmail.com>
2017-10-29 17:35:17 -0700

    2.0.0

--- actions.php
+++ actions.php
@@ -1,63 +1,62 @@
 <?php
 
-require_once("../../global/session_start.php");
-require_once(dirname(__FILE__) . "/library.php");
-ft_check_permission("user");
+require_once("../../global/library.php");
 
-$request = array_merge($_POST, $_GET);
+use FormTools\Modules;
 
-$return_str = "";
-if (isset($request["return_vars"]))
-{
-  $vals = array();
-  while (list($key, $value) = each($request["return_vars"]))
-  {
-    $vals[] = "\"$key\": \"$value\"";
-  }
-  $return_str = ", " . implode(", ", $vals);
+$module = Modules::initModulePage("client");
+
+
+switch ($request["action"]) {
+
+    // called by the administrator or client on the Edit Submission page. Note that we pull the submission ID
+    // and the form ID from sessions rather than have them explictly passed by the JS. This is a security precaution -
+    // it prevents a potential hacker exploiting this function here. Instead they'd have to set the sessions by another
+    // route which is trickier
+    case "delete_submission_file":
+        $form_id       = $request["form_id"];
+        $submission_id = $request["submission_id"];
+        $field_id      = $request["field_id"];
+        $force_delete  = ($request["force_delete"] == "true") ? true : false;
+
+        // TODO beef up the security here. Check that the person logged in is permitted to see this submission & field...
+
+        list ($success, $message) = $module->deleteFileSubmission($form_id, $submission_id, $field_id, $force_delete);
+        output_json_with_return_vars(array(
+            "success" => ($success) ? 1 : 0,
+            "message" => $message
+        ));
+        break;
+
+    // this is called when the field type is being used in the Form Builder. This is just slightly more restrictive than
+    // the logged-in context: it pulls the form ID and submission ID from sessions instead of from the page (which could
+    // be hacked)
+    case "delete_submission_file_standalone":
+        $published_form_id = (isset($request["published_form_id"])) ? $request["published_form_id"] : "";
+
+        if (empty($published_form_id)) {
+            output_json_with_return_vars(array(
+                "success" => 0,
+                "message" => "Your form is missing the form_tools_published_form_id ID field."
+            ));
+            exit;
+        }
+        $form_id       = $_SESSION["form_builder_{$published_form_id}"]["form_tools_form_id"];
+        $submission_id = $_SESSION["form_builder_{$published_form_id}"]["form_tools_submission_id"];
+        $field_id      = $request["field_id"];
+        $force_delete  = ($request["force_delete"] == "true") ? true : false;
+
+        list ($success, $message) = $module->deleteFileSubmission($form_id, $submission_id, $field_id, $force_delete);
+        output_json_with_return_vars(array(
+            "success" => 0,
+            "message" => $message
+        ));
+        break;
 }
 
 
-switch ($request["action"])
+function output_json_with_return_vars($data)
 {
-  // called by the administrator or client on the Edit Submission page. Note that we pull the submission ID
-  // and the form ID from sessions rather than have them explictly passed by the JS. This is a security precaution -
-  // it prevents a potential hacker exploiting this function here. Instead they'd have to set the sessions by another
-  // route which is trickier
-  case "delete_submission_file":
-    $form_id       = $request["form_id"];
-    $submission_id = $request["submission_id"];
-    $field_id      = $request["field_id"];
-    $force_delete  = ($request["force_delete"] == "true") ? true : false;
-
-    // TODO beef up the security here. Check that the person logged in is permitted to see this submission & field...
-
-    list($success, $message) = ft_file_delete_file_submission($form_id, $submission_id, $field_id, $force_delete);
-    $success = ($success) ? 1 : 0;
-    $message = ft_sanitize($message);
-    $message = preg_replace("/\\\'/", "'", $message);
-    echo "{ \"success\": \"$success\", \"message\": \"$message\" {$return_str} }";
-    break;
-
-  // this is called when the field type is being used in the Form Builder. This is just slightly more restrictive than
-  // the logged-in context: it pulls the form ID and submission ID from sessions instead of from the page (which could
-  // be hacked)
-  case "delete_submission_file_standalone":
-    $published_form_id = (isset($request["published_form_id"])) ? $request["published_form_id"] : "";
-    if (empty($published_form_id))
-    {
-      echo "{ \"success\": \"0\", \"message\": \"Your form is missing the form_tools_published_form_id ID field.\" {$return_str} }";
-      exit;
-    }
-    $form_id       = $_SESSION["form_builder_{$published_form_id}"]["form_tools_form_id"];
-    $submission_id = $_SESSION["form_builder_{$published_form_id}"]["form_tools_submission_id"];
-    $field_id      = $request["field_id"];
-    $force_delete  = ($request["force_delete"] == "true") ? true : false;
-
-    list($success, $message) = ft_file_delete_file_submission($form_id, $submission_id, $field_id, $force_delete);
-    $success = ($success) ? 1 : 0;
-    $message = ft_sanitize($message);
-    $message = preg_replace("/\\\'/", "'", $message);
-    echo "{ \"success\": \"$success\", \"message\": \"$message\" {$return_str} }";
-    break;
+    global $request;
+    echo json_encode(array_merge($request["return_vars"], $data));
 }
--- module_config.php
+++ module_config.php
@@ -1,75 +1,78 @@
 <?php
 
+
 $STRUCTURE = array();
 $HOOKS = array(
-  array(
-    "hook_type"       => "code",
-    "action_location" => "manage_files",
-    "function_name"   => "ft_update_submission",
-    "hook_function"   => "ft_file_update_submission_hook",
-    "priority"        => "50"
-  ),
-  array(
-    "hook_type"       => "code",
-    "action_location" => "manage_files",
-    "function_name"   => "ft_process_form",
-    "hook_function"   => "ft_file_process_form_hook",
-    "priority"        => "50"
-  ),
-  array(
-    "hook_type"       => "code",
-    "action_location" => "manage_files",
-    "function_name"   => "ft_api_process_form",
-    "hook_function"   => "ft_file_api_process_form_hook",
-    "priority"        => "50"
-  ),
-  array(
-    "hook_type"       => "code",
-    "action_location" => "start",
-    "function_name"   => "ft_delete_submission_files",
-    "hook_function"   => "ft_file_delete_submissions_hook",
-    "priority"        => "50"
-  ),
-  array(
-    "hook_type"       => "code",
-    "action_location" => "start",
-    "function_name"   => "ft_get_uploaded_files",
-    "hook_function"   => "ft_file_get_uploaded_files_hook",
-    "priority"        => "50"
-  ),
-  array(
-    "hook_type"       => "template",
-    "action_location" => "head_bottom",
-    "function_name"   => "",
-    "hook_function"   => "ft_file_include_js",
-    "priority"        => "50"
-  ),
-  array(
-    "hook_type"       => "template",
-    "action_location" => "standalone_form_fields_head_bottom",
-    "function_name"   => "",
-    "hook_function"   => "ft_file_include_standalone_js",
-    "priority"        => "50"
-  )
+    array(
+        "hook_type"       => "code",
+        "action_location" => "manage_files",
+        "function_name"   => "FormTools\\Submissions::updateSubmission",
+        "hook_function"   => "updateSubmissionHook",
+        "priority"        => "50"
+    ),
+    array(
+        "hook_type"       => "code",
+        "action_location" => "manage_files",
+        "function_name"   => "FormTools\\Submissions::processFormSubmission",
+        "hook_function"   => "processFormSubmissionHook",
+        "priority"        => "50"
+    ),
+    array(
+        "hook_type"       => "code",
+        "action_location" => "manage_files",
+        "function_name"   => "FormTools\\API::processFormSubmission",
+        "hook_function"   => "apiProcessFormSubmissionHook",
+        "priority"        => "50"
+    ),
+    array(
+        "hook_type"       => "code",
+        "action_location" => "start",
+        "function_name"   => "FormTools\\Files::deleteSubmissionFiles",
+        "hook_function"   => "deleteSubmissionsHook",
+        "priority"        => "50"
+    ),
+    array(
+        "hook_type"       => "code",
+        "action_location" => "start",
+        "function_name"   => "FormTools\\Fields::getUploadedFiles",
+        "hook_function"   => "getUploadedFilesHook",
+        "priority"        => "50"
+    ),
+    array(
+        "hook_type"       => "template",
+        "action_location" => "head_bottom",
+        "function_name"   => "",
+        "hook_function"   => "includeJs",
+        "priority"        => "50"
+    ),
+    array(
+        "hook_type"       => "template",
+        "action_location" => "standalone_form_fields_head_bottom",
+        "function_name"   => "",
+        "hook_function"   => "includeStandaloneJs",
+        "priority"        => "50"
+    )
 );
+
 $FILES = array(
-  "actions.php",
-  "database_integrity.php",
-  "images/",
-  "images/file_upload_icon.png",
-  "images/index.html",
-  "index.php",
-  "lang/",
-  "lang/en_us.php",
-  "lang/index.html",
-  "library.php",
-  "module.php",
-  "module_config.php",
-  "scripts/",
-  "scripts/edit_submission.js",
-  "scripts/index.html",
-  "scripts/standalone.js",
-  "templates/",
-  "templates/index.tpl",
-  "templates/index.html"
+    "actions.php",
+    "code",
+    "code/Module.class.php",
+    "images/",
+    "images/file_upload_icon.png",
+    "images/index.html",
+    "index.php",
+    "lang/",
+    "lang/en_us.php",
+    "lang/index.html",
+    "library.php",
+    "module_config.php",
+    "README.md",
+    "scripts/",
+    "scripts/edit_submission.js",
+    "scripts/index.html",
+    "scripts/standalone.js",
+    "templates/",
+    "templates/index.tpl",
+    "templates/index.html"
 );
