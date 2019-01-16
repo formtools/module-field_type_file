3012ae8f4a0cae6657a205ae6de6df81cbf26a78
Ben Keen <ben.keen@gmail.com>
2019-01-15 22:18:41 -0800

    5 more translations

--- lang/fa.php
+++ lang/fa.php
@@ -1,15 +1,15 @@
 <?php
 
 $L = array();
-$L["module_name"] = "File Upload";
+$L["module_name"] = "آپلود فایل";
 
-$L["confirm_delete_submission_files"] = "Are you sure you want to delete these files?";
-$L["module_description"] = "This module provides a simple file upload field for use in your Form Tools fields.";
-$L["notify_file_deleted"] = "The file has been deleted.";
+$L["confirm_delete_submission_files"] = "آیا مطمئن هستید که می خواهید این فایل ها را حذف کنید؟";
+$L["module_description"] = "این ماژول یک فیلد آپلود فایل ساده برای استفاده در فیلدهای ابزار فرم شما فراهم می کند.";
+$L["notify_file_deleted"] = "پرونده حذف شده است";
 $L["notify_file_too_large"] = "The <b>{\$filename}</b> file is too large. The file was {\$file_size}KB, but the maximum permitted file upload size is {\$max_file_size}KB.";
-$L["notify_files_deleted"] = "The files have been deleted.";
+$L["notify_files_deleted"] = "فایل ها حذف شده اند.";
 $L["notify_files_too_large"] = "The following files are too large: <b>{\$file_list}</b>";
-$L["notify_submission_no_field_id"] = "This submission's field ID was not found.";
+$L["notify_submission_no_field_id"] = "این شناسه فیلد ارسالی یافت نشد";
 $L["notify_file_not_deleted_invalid_permissions"] = "The <b>{\$filename}</b> file in the <b>{\$folder}</b> folder could not be deleted because it didn't have the appropriate permissions.";
 $L["notify_files_not_deleted_invalid_permissions"] = "The following files could not be deleted because they don't have the appropriate permissions: <b>{\$file_list}</b>.";
 $L["notify_file_not_deleted_unknown_error"] = "The <b>{\$filename}</b> file in the <b>{\$folder}</b> folder could not be deleted due to an unknown error.";
@@ -18,9 +18,9 @@ $L["notify_clear_error"] = "<a href=\"#\" onclick=\"{\$js_link}\">Click here</a>
 $L["notify_clear_errors"] = "<a href=\"#\" onclick=\"{\$js_link}\">Click here</a> to ignore these error messages and just remove the references from the database.";
 $L["notify_file_deleted_with_error"] = "<b>1</b> file was successfully deleted, but the following errors occurred";
 $L["notify_files_deleted_with_error"] = "<b>{\$num_files}</b> files were successfully deleted, but the following errors occurred";
-$L["notify_field_type_reset"] = "The field type has been reset.";
+$L["notify_field_type_reset"] = "نوع فیلد تنظیم مجدد شده است.";
 $L["notify_submission_updated_file_problems"] = "Your submission was updated, but we encountered problems:";
-$L["notify_upload_invalid_file_extension"] = "The file being uploaded has an unsupported file extension.";
+$L["notify_upload_invalid_file_extension"] = "تسلیم شما به روز شد، اما با مشکلات مواجه شدیم:";
 $L["notify_file_not_deleted_missing"] = "The <b>{\$file}</b> file hasn't been deleted because it doesn't exist in the expected folder (<b>{\$folder}</b>)";
 $L["notify_files_not_deleted_missing"] = "The following files haven't been deleted because they doesn't exist in the expected folder (<b>{\$folder}</b>): <b>{\$file_list}</b>";
 $L["notify_upload_invalid_file_extensions"] = "The following files have unsupported file extensions for their field: <b>{\$file_list}</b>";
@@ -32,9 +32,9 @@ $L["notify_file_incorrect_permissions"] = "The <b>{\$filename}</b> file in the <
 $L["notify_files_incorrect_permissions"] = "These files could not be removed due to file permissions: <b>{\$file_list}</b>";
 $L["notify_file_unknown_reasons"] = "The <b>{\$filename}</b> file in the <b>{\$folder}</b> folder could not be removed for unknown reasons.";
 $L["notify_files_unknown_reasons"] = "These files could not be removed due to unknown reasons: <b>{\$file_list}</b>";
-$L["phrase_reset_field_type"] = "Reset Field Type";
-$L["phrase_no_files_to_delete"] = "No files to delete.";
+$L["phrase_reset_field_type"] = "نوع فیلد را بازنشانی کنید";
+$L["phrase_no_files_to_delete"] = "هیچ فایلی برای حذف وجود ندارد.";
 $L["text_help"] = "For more information on this module, please see the <a href=\"http://modules.formtools.org/field_type_tinymce/\" target=\"_blank\">help documentation</a> on the Form Tools site.";
-$L["text_reset_field_type_desc"] = "This button below lets you reset this field type to the latest factory defaults. Generally you don't have to do this, but in case of a failed upgrade this is a failsafe way to ensure it's up to date.";
+$L["text_reset_field_type_desc"] = "این دکمه زیر اجازه می دهد تا این نوع فیلد را به آخرین پیش فرض های کارخانه بازنشانی کنید. به طور کلی شما مجبور نیستید این کار را انجام دهید، اما در صورت ارتقاء ناموفق، این یک راه امن برای اطمینان از آن است که تا به امروز.";
 $L["text_intro_desc"] = "Use the <a href=\"{\$link}\">Settings &raquo; Files</a> page to define the default file upload settings. You can override those settings by editing any form field via the Edit Form &raquo; Fields tab.";
-$L["word_help"] = "Help";
+$L["word_help"] = "کمک";
